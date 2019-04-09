a5371c7cd46fd047f63d0e0f00b7bd3a6b73f5d9
Martin Scharm <martin@binfalse.de>
2019-04-09 19:18:41 +0200

    regenerated cluster images

--- res/2019-eu/analyse/cluster-votes/cluster-abstimmungen-from-individuals.R
+++ res/2019-eu/analyse/cluster-votes/cluster-abstimmungen-from-individuals.R
@@ -16,6 +16,11 @@ m[m==-1]=NA
 # cluster and plot the data using gplot's heatmap.2 tool
 pdf ("abstimmungscluster-from-individuals.pdf", height=20)
 colfunc <- colorRampPalette(c("#FF0000FF", "#FFFF00FF"))
-heatmap.2 (m, tracecol=1, cexCol=.9, margins=c(12,17),
+heatmap.2 (m, tracecol=1, cexCol=.9, margins=c(7,13),
+           na.rm=TRUE, col=colfunc(15), na.color="#DDDDDDFF")
+dev.off ()
+
+png ("abstimmungscluster-from-individuals.png", height=1100)
+heatmap.2 (m, tracecol=1, cexCol=.9, key=F, margins=c(7,13),
            na.rm=TRUE, col=colfunc(15), na.color="#DDDDDDFF")
 dev.off ()
