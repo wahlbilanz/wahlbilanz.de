f05d7bd5beb83435b1669eea013d485ae8749d65
Martin Scharm <martin@binfalse.de>
2019-04-09 19:00:46 +0200

    re-analysing individuals' data

---
+++ res/2019-eu/analyse/cluster-votes/cluster-abstimmungen-from-individuals.R
@@ -0,0 +1,21 @@
+library(gplots)
+library(RColorBrewer)
+
+# read the data
+votes <- read.table (file="../../wrangling/summary-from-individuals.table", header=TRUE, sep="\t")
+
+# coloumn 1 is just the id
+m=as.matrix (votes[,2:16])
+# but we can use the ids as rownames :)
+rownames(m)<-votes[,1]
+
+
+
+m[m==-1]=NA
+
+# cluster and plot the data using gplot's heatmap.2 tool
+pdf ("abstimmungscluster-from-individuals.pdf", height=20)
+colfunc <- colorRampPalette(c("#FF0000FF", "#FFFF00FF"))
+heatmap.2 (m, tracecol=1, cexCol=.9, margins=c(12,17),
+           na.rm=TRUE, col=colfunc(15), na.color="#DDDDDDFF")
+dev.off ()
